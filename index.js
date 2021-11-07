ab5dce53d0c3b1c2636be1786b8e4113c6221557
Maximo Fernandez Riera <mfernandez@cifpfbmoll.eu>
2021-11-07 10:58:42 +0100

    N elementos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Calculate = {
 	sum(inputArray) {
     
+    //Implementamos la suma de elementos
     let totalSum = 0;
     const inputArrayLength = inputArray.length;
 		for (let i = 0; i < inputArrayLength; i++){
